0d8e4a7de00f338c99f6ff4bd8b36ea7ef86505b
NathanKeith21 <66037872+NathanKeith21@users.noreply.github.com>
2021-03-16 09:05:47 -0600

    Update SignUpInfo.cs

--- IS403-Project1/Models/SignUpInfo.cs
+++ IS403-Project1/Models/SignUpInfo.cs
@@ -1,11 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace IS403_Project1.Models
 {
     public class SignUpInfo
     {
+        [Key]
+        [Required]
+        public int GroupId { get; set; }
+        [Required]
+        public string GroupName { get; set; }
+        [Required]
+        public int GroupSize { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
     }
 }
